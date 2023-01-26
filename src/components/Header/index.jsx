9ad4bbc185f3f9e3440493cc0299ff6da05cd104
Thiago Durães <duraes.thiago@gmail.com>
2023-01-26 19:21:36 -0300

    handleSubmit on Header updated

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,9 +7,11 @@ export function Header({ onAddTask }) {
   const [taskText, setTaskText] = useState('');
 
   function handleSubmit(event) {
+    event.preventDefault();
     if (taskText) {
-      event.preventDefault();
       onAddTask(taskText);
+      setTaskText('');
+    } else {
       setTaskText('');
     }
   }
